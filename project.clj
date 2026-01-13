07152857450070ea915bf38a11d368388d65959f
Rafal Dittwald <rafal.dittwald@gmail.com>
2026-01-13 14:31:39 -0500

    Release 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.rafd/sys "0.1.0"
+(defproject com.github.rafd/sys "0.2.0"
   :description "A boring dependency injection system for Clojure(Script) apps."
   :url "https://github.com/rafd/sys"
   :license {:name "MIT"}
